187539032119d13e4c59149e2f4d18525fa54ed0
T3tsuo <comeautakumi@gmail.com>
2021-05-12 19:40:44 -0300

    optimized dataframe

--- dataset to csv/data_to_csv.py
+++ dataset to csv/data_to_csv.py
@@ -5,8 +5,8 @@ import pandas as pd
 import arez
 import asyncio
 
-DEV_ID = 0  # your Developer ID
-AUTH_KEY = ""  # your Auth Key
+DEV_ID = 3733  # your Developer ID
+AUTH_KEY = "DDF9D77BBE2F4D66908BA307D850CDD3"  # your Auth Key
 
 
 async def get_champion(list, name):
@@ -27,12 +27,10 @@ async def main():
     matchdata.to_csv('match_data_temp.csv', mode='w', index=False)
     teamcolumns1 = ["id", "champ1", "champ2", "champ3", "champ4", "champ5", "cwinrate1", "cwinrate2", "cwinrate3",
                    "cwinrate4", "cwinrate5", "ckda1", "ckda2", "ckda3", "ckda4", "ckda5", "cdf1", "cdf2", "cdf3",
-                   "cdf4", "cdf5", "alvl1", "alvl2", "alvl3", "alvl4", "alvl5", "awinrate1", "awinrate2", "awinrate3",
-                   "awinrate4", "awinrate5", "kda1", "kda2", "kda3", "kda4", "kda5"]
+                   "cdf4", "cdf5"]
     teamcolumns2 = ["id", "champ6", "champ7", "champ8", "champ9", "champ10", "cwinrate6", "cwinrate7", "cwinrate8",
                     "cwinrate9", "cwinrate10", "ckda6", "ckda7", "ckda8", "ckda9", "ckda10", "cdf6", "cdf7", "cdf8",
-                    "cdf9", "cdf10", "alvl6", "alvl7", "alvl8", "alvl9", "alvl10", "awinrate6", "awinrate7",
-                    "awinrate8", "awinrate9", "awinrate10", "kda6", "kda7", "kda8", "kda9", "kda10"]
+                    "cdf9", "cdf10"]
     team1data = pd.DataFrame(columns=teamcolumns1)
     team1data.to_csv('team1_data_temp.csv', mode='w', index=False)
     team2data = pd.DataFrame(columns=teamcolumns2)
@@ -44,16 +42,10 @@ async def main():
             cwinrate1 = []
             ckda1 = []
             cdf1 = []
-            alvl1 = []
-            awinrate1 = []
-            akda1 = []
             champ2 = []
             cwinrate2 = []
             ckda2 = []
             cdf2 = []
-            alvl2 = []
-            awinrate2 = []
-            akda2 = []
             # add match data to dataframe
             for i in range(5):
                 # grabs every champion name
@@ -63,9 +55,6 @@ async def main():
                 cwinrate1.append(champions_stat.winrate)
                 ckda1.append(champions_stat.kda2)
                 cdf1.append(champions_stat.df)
-                alvl1.append(match.team1[i].account_level)
-                awinrate1.append(match.team1[i].player.casual.winrate)
-                akda1.append(match.team1[i].kda2)
 
                 # grabs every champion name
                 champ2.append(match.team2[i].champion.name)
@@ -75,20 +64,13 @@ async def main():
                 cwinrate2.append(champions_stat.winrate)
                 ckda2.append(champions_stat.kda2)
                 cdf2.append(champions_stat.df)
-                alvl2.append(match.team2[i].account_level)
-                awinrate2.append(match.team2[i].player.casual.winrate)
-                akda2.append(match.team2[i].kda2)
             # add teams data to dataframe
             team1data.loc[0] = [match.id, champ1[0], champ1[1], champ1[2], champ1[3], champ1[4], cwinrate1[0],
                                 cwinrate1[1], cwinrate1[2], cwinrate1[3], cwinrate1[4], ckda1[0], ckda1[1],
-                                ckda1[2], ckda1[3], ckda1[4], cdf1[0], cdf1[1], cdf1[2], cdf1[3], cdf1[4], alvl1[0],
-                                alvl1[1], alvl1[2], alvl1[3], alvl1[4], awinrate1[0], awinrate1[1], awinrate1[2],
-                                awinrate1[3], awinrate1[4], akda1[0], akda1[1], akda1[2], akda1[3], akda1[4]]
+                                ckda1[2], ckda1[3], ckda1[4], cdf1[0], cdf1[1], cdf1[2], cdf1[3], cdf1[4]]
             team2data.loc[0] = [match.id, champ2[0], champ2[1], champ2[2], champ2[3], champ2[4], cwinrate2[0],
                                 cwinrate2[1], cwinrate2[2], cwinrate2[3], cwinrate2[4], ckda2[0], ckda2[1],
-                                ckda2[2], ckda2[3], ckda2[4], cdf2[0], cdf2[1], cdf2[2], cdf2[3], cdf2[4], alvl2[0],
-                                alvl2[1], alvl2[2], alvl2[3], alvl2[4], awinrate2[0], awinrate2[1], awinrate2[2],
-                                awinrate2[3], awinrate2[4], akda2[0], akda2[1], akda2[2], akda2[3], akda2[4]]
+                                ckda2[2], ckda2[3], ckda2[4], cdf2[0], cdf2[1], cdf2[2], cdf2[3], cdf2[4]]
             matchdata.loc[0] = [match.id, match.map_name, match.winning_team, match.duration]
             matchdata.to_csv('match_data_temp.csv', mode='a', header=False, index=False)
             team1data.to_csv('team1_data_temp.csv', mode='a', header=False, index=False)
